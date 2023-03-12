97ee50b449aeb1e8b494b20b63aeac7244bd1e08
thirumurugan7 <thiru82003@gmail.com>
2023-03-12 14:04:09 +0530

    loader component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { Suspense, useEffect, useState } from "react";
-
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
+
 const Computers = ({ isMobile }) => {
-  //importing the 3d model using useGLTF
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -36,26 +33,31 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width:500px)");
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
+    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
+    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
+    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
-  //position:[x,y,z]
-  //fov:field of view
-  //mAx and min polar angle specify which direction the object should rotate
+
   return (
     <Canvas
       frameloop="demand"
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
